99972e74583c318d24a5ef0fe61158beb78e4e51
apierce1992 <apierce1992@gmail.com>
2016-01-14 09:30:43 -0500

    added comments in to explain basic concepts in code

--- echo.pl
+++ echo.pl
@@ -1,6 +1,18 @@
 #!/usr/bin/perl
 use strict;
+use warnings;
+
+#print "@ARGV\n";
+
+#example of subroutines
+sub echo {
+my @params = @_;
+print "$params[0] $params[2]\n";
+my @word = $params[0];
+print "$word\n";
+}
+
+echo @ARGV;
 
 
-
-print "@ARGV\n";
+#example of list literals
